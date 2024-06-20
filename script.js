feded6fd08a6a0818091a56d7b90421dab8f1051
Dovudxon005 <154241506+Dovudxon005@users.noreply.github.com>
2024-06-20 18:09:32 +0500

    gulbahor

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.button').addEventListener('click', function (){
+    if()
+    a = document.querySelector('.ekran')
+    a.value = parseInt( a.value) +1
+    console.log(a.value);
+})
+
+document.querySelector('.delete').addEventListener('click', function(){
+    a = document.querySelector('.ekran')
+    ochirish = 0
+    a.value = ochirish
+    console.log (ochirish);
+})
